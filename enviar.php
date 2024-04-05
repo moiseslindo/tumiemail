3d7241b676d4ab13e056b9c5b44db7f795bb91cd
Moises Lindo <mlindog@atento.com.pe>
2024-04-05 15:10:34 -0500

    .

--- enviar.php
+++ enviar.php
@@ -7,9 +7,9 @@ $mail->Port = 587;
 $mail->SMTPAuth = true;
 $mail->Username = 'MS_a6P8ku@trial-0p7kx4xq93eg9yjr.mlsender.net';
 $mail->Password = '0L8S9hLURctlXDmH';
-$mail->setFrom('MS_a6P8ku@trial-0p7kx4xq93eg9yjr.mlsender.net', 'yo mismo');
+$mail->setFrom('MS_a6P8ku@trial-0p7kx4xq93eg9yjr.mlsender.net', 'Moises Lindo Gutarra');
 $mail->addAddress('mlindo@gmail.com', 'Moises Lindo');
-$mail->Subject = 'Prueba de envio de emails';
+$mail->Subject = 'Prueba de envio de emails desde mailersend';
 $mail->Body = "Hola a todos!!";
 if (!$mail->send()) {
     echo 'Email could not be sent.';
