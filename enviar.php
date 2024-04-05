94a276c4ccf8d8bc5ec8a3165ebc68c91df83f40
Moises Lindo <mlindog@atento.com.pe>
2024-04-05 15:08:31 -0500

    Tumi 20240405

---
+++ enviar.php
@@ -0,0 +1,20 @@
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Host = 'smtp.mailersend.net';
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->Username = 'MS_a6P8ku@trial-0p7kx4xq93eg9yjr.mlsender.net';
+$mail->Password = '0L8S9hLURctlXDmH';
+$mail->setFrom('MS_a6P8ku@trial-0p7kx4xq93eg9yjr.mlsender.net', 'yo mismo');
+$mail->addAddress('mlindo@gmail.com', 'Moises Lindo');
+$mail->Subject = 'Prueba de envio de emails';
+$mail->Body = "Hola a todos!!";
+if (!$mail->send()) {
+    echo 'Email could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Email was successfully sent.';
+}
+?>
