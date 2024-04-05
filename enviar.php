d1db62140127c3d9011de75c7101ea26e753e56f
Moises Lindo <mlindog@atento.com.pe>
2024-04-05 15:25:32 -0500

    .

--- enviar.php
+++ enviar.php
@@ -9,7 +9,7 @@ $mail->Username = 'MS_a6P8ku@trial-0p7kx4xq93eg9yjr.mlsender.net';
 $mail->Password = '0L8S9hLURctlXDmH';
 $mail->setFrom('MS_a6P8ku@trial-0p7kx4xq93eg9yjr.mlsender.net', 'Moises Lindo The Greatest');
 $mail->addAddress('camilo.quintana10@gmail.com', 'Camilo');
-$mail->addAddress('mlindo@gmail.com', 'Moises');
+//$mail->addAddress('mlindo@gmail.com', 'Moises');
 $mail->addReplyTo('rrodriguezm98@gmail.com', 'Richard');
 $mail->Subject = 'Prueba de envio de emails desde mailersend';
 $mail->Body = "Hola a todos!!, ya tenemos mensajeria, bajar el proyecto desde: https://github.com/moiseslindo/tumiemail.git";
